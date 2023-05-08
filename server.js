48fd18cc2130e4fa57fa0aae8ca01864a2a4747b
Kevin Islas <kevinislas04@gmail.com>
2023-05-08 00:40:21 -0700

    changes

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const createError = require("http-errors");
 const cookieParser = require("cookie-parser");
 const morgan = require("morgan");
 const express = require("express");
+import livereload from "livereload";
+import connectLiveReload from "connect-livereload";
 const testRoutes = require("./routes/test/index.js");
 
 require("dotenv").config();
@@ -13,10 +15,17 @@ app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+// if (process.env.NODE_ENV === "development") {
+//   const liveReloadServer = livereload.createServer();
+//   liveReloadServer.watch(path.join(".", "backend", "static"));
+//   liveReloadServer.server();
+// }
+
 app.set("views", path.join(__dirname, "backend", "views"));
 app.set("view engine", "pug");
 app.use(express.static(path.join(__dirname, "backend", "static")));
-app.use("/test",testRoutes)
+app.use("/test", testRoutes);
 
 const rootRoutes = require("./backend/routes/root");
 app.use("/", rootRoutes);
