5418cfa2a0bda6fa477dd373df367e104b7f36de
Kevin I <kevinislands@Kevins-MacBook-Pro.local>
2023-05-19 23:02:56 -0700

    sessions migrations

--- shared/constants.js
+++ shared/constants.js
@@ -1,4 +1,4 @@
 const GAME_CREATED = "game:created";
-// const GAME_UPDATED = (id) => `game:${id}:updated`;
+const GAME_UPDATED = (id) => `game:${id}:updated`;
 
-module.exports = { GAME_CREATED };
+module.exports = { GAME_CREATED, GAME_UPDATED };
